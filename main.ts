68728b8d0f4ce51d33d3b37c43ac075efd24d6f8
HacKids Education <65713501+HacKids-Edu@users.noreply.github.com>
2020-08-05 16:27:18 -0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    volume += -10
+})
+input.onButtonPressed(Button.B, function () {
+    volume += 10
+})
 let nota = 0
 music.setTempo(20)
+let volume = 100
 basic.forever(function () {
+    led.plotBarGraph(
+    volume,
+    255
+    )
+    music.setVolume(volume)
     nota = pins.map(
     input.lightLevel(),
     0,
